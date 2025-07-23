d39ea6eea49a20e4be1a1776cca7c4415ee9e460
Mick Jari Zilverentant <jari.zilverentant@gmail.com>
2025-07-23 16:00:21 +0200

    remove comments

--- src/Utils/ctz.h
+++ src/Utils/ctz.h
@@ -1,13 +1,12 @@
 #pragma once
 #include <cstdint>
-#include <stdexcept>
+#include <cassert>
 
 // Basically the same as __builtin_ctzll
 constexpr unsigned int ctz(uint64_t x) {
-    if (x == 0) {
-        throw std::invalid_argument("Input to ctz cannot be zero.");
-    }
-    int n = 0;
+    assert(x != 0 && "ctz called with zero");
+
+    unsigned int n = 0;
     while ((x & 1) == 0) {
         x >>= 1;
         n++;
